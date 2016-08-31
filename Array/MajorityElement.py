a00a6403a8d8a6b765c63734ba6f6da433f9bc65
pengshuang <pengshuang92@163.com>
2016-08-31 21:03:26 +0800

    添加注释

--- Array/MajorityElement.py
+++ Array/MajorityElement.py
@@ -23,6 +23,7 @@ class Solution(object):
                 candidate1, count1 = n, 1
             elif count2 == 0:
                 candidate2, count2 = n, 1
+            # 为了统计出现次数最多的2个数
             else:
                 count1, count2 = count1 - 1, count2 - 1
         return [n for n in (candidate1, candidate2) if nums.count(n) > len(nums) // 3]
