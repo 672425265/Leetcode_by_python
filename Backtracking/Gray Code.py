940b596c592fa1f2ae0c27f4c46a6498b3bff80d
pengshuang <pengshuang92@163.com>
2016-11-08 10:57:21 +0800

    热身

---
+++ Backtracking/Gray Code.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+'''
+The gray code is a binary numeral system where two successive values differ in only one bit.
+Given a non-negative integer n representing the total number of bits in the code,
+print the sequence of gray code.
+A gray code sequence must begin with 0.
+For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+00 - 0
+01 - 1
+11 - 3
+10 - 2
+Note:
+For a given n, a gray code sequence is not uniquely defined.
+For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
+For now, the judge is able to judge based on one instance of gray code sequence.
+'''
+
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        if n == 0:
+            return [0]
+        result = self.grayCode(n-1)
+        addNumber = 1 << (n-1)
+        originalsize = len(result)
+        for i in range(originalsize-1, -1, -1):
+            result.append(addNumber + result[i])
+        return result
+
+solution = Solution()
+print solution.grayCode(2)
