36b9171d22e83114e70db658db5bba9156b058f3
pengshuang <pengshuang92@163.com>
2016-11-14 14:29:10 +0800

    复出首题

---
+++ String/Ransom Note.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+'''
+Given an arbitrary ransom note string and another string containing letters from all the magazines,
+write a function that will return true if the ransom note can be constructed from the magazines ;
+otherwise, it will return false.
+Each letter in the magazine string can only be used once in your ransom note.
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+'''
+from collections import Counter
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        ransomCnt = Counter(ransomNote)
+        magazineCnt = Counter(magazine)
+        return not ransomCnt - magazineCnt
+
+solution = Solution()
+print solution.canConstruct("aa", "ab")
