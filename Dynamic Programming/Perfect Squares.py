907a2a6989285ede271615b1b8765b81b1075ef3
pengshuang <pengshuang92@163.com>
2016-11-16 09:55:41 +0800

    dp 问题

--- Dynamic Programming/Perfect Squares.py
+++ Dynamic Programming/Perfect Squares.py
@@ -5,15 +5,12 @@ perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
 For example, given n = 12, return 3 because 12 = 4 + 4 + 4;
 given n = 13, return 2 because 13 = 4 + 9.
 '''
-import collections
-
 class Solution:
     def numSquares(self, n):
         while n % 4 == 0:
             n /= 4
         if n % 8 == 7:
             return 4
-
         for i in xrange(n+1):
             temp = i * i
             if temp <= n:
