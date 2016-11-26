fd7fef8ee34a15d6ce795403103342423cda6f11
pengshuang <pengshuang92@163.com>
2016-11-26 09:56:05 +0800

    复习

---
+++ HashTable/Island Perimeter.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+'''
+You are given a map in form of a two-dimensional integer grid where 1 represents land
+and 0 represents water. Grid cells are connected horizontally/vertically (not diagonally).
+The grid is completely surrounded by water,
+and there is exactly one island (i.e., one or more connected land cells).
+The island doesn't have "lakes" (water inside that isn't connected to the water around the island).
+One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100.
+Determine the perimeter of the island.
+
+Example:
+
+[[0,1,0,0],
+ [1,1,1,0],
+ [0,1,0,0],
+ [1,1,0,0]]
+
+Answer: 16
+Explanation: The perimeter is the 16 yellow stripes in the image below:
+'''
+
+'''
+每一个陆地单元格的周长为4，当两单元格上下或者左右相邻时，令周长减2。
+'''
+
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        h = len(grid)
+        w = len(grid[0]) if h else 0
+        ans = 0
+        for x in range(h):
+            for y in range(w):
+                if grid[x][y] == 1:
+                    ans += 4
+                    if x > 0 and grid[x - 1][y]:
+                        ans -= 2
+                    if y > 0 and grid[x][y - 1]:
+                        ans -= 2
+        return ans
