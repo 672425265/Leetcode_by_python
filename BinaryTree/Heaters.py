071d6f1984ee4aa9126e4e86856d03031813d6ad
pengshuang <pengshuang92@163.com>
2016-12-14 11:00:09 +0800

    恢复状态

--- BinaryTree/Heaters.py
+++ BinaryTree/Heaters.py
@@ -23,10 +23,12 @@ Explanation: The two heater was placed in the position 1 and 4. We need to use r
 '''
 
 '''
-房屋和加热器的数目均为非负整数，并且不大于25000
-房屋和加热器的位置是非负整数，并且不超过10^9
-位于加热器半径范围内的房屋均可被加热
-加热器的半径都是一样的
+排序（Sort） + 二分查找（Binary Search）
+升序排列加热器的坐标heaters
+遍历房屋houses，记当前房屋坐标为house：
+    利用二分查找，分别找到不大于house的最大加热器坐标left，以及不小于house的最小加热器坐标right
+    则当前房屋所需的最小加热器半径radius = min(house - left, right - house)
+    利用radius更新最终答案ans
 '''
 
 import bisect
@@ -41,7 +43,7 @@ class Solution(object):
         ans = 0
         heaters.sort()
         for house in houses:
-            radius = 0x7FFFFFF
+            radius = 0x7FFFFFFF
             le = bisect.bisect_right(heaters, house)
             if le > 0:
                 radius = min(radius, house - heaters[le - 1])
