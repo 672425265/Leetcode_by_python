f4ec482e1ce2bce3c0f54cd606e9267e7b11a63d
pengshuang <pengshuang92@163.com>
2017-01-23 10:40:28 +0800

    恢复状态

--- Array/FindPeakElement.py
+++ Array/FindPeakElement.py
@@ -25,10 +25,8 @@ class Solution(object):
 
         while left < right:
             mid = (left+right)/2
-            if nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
-                return mid
-            elif nums[mid] < nums[mid+1]:
-                right = mid - 1
-            elif nums[mid] < nums[mid-1]:
+            if nums[mid] < nums[mid+1]:
                 left = mid + 1
-        return left
+            else:
+                right = mid
+        return right
