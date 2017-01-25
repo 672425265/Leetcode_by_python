2a30ba1112c4bb02851eeb34f558478bff9588ae
pengshuang <pengshuang92@163.com>
2017-01-25 09:40:29 +0800

    恢复状态

---
+++ math/PowerofTwo.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        preN = n - 1
+        x = n & preN
+        return x == 0
