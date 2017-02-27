f9fd2d2fd4767105689e7efd143aa754d11d084b
pengshuang <pengshuang92@163.com>
2017-02-27 08:54:41 +0800

    链表总结

--- Summary/LinkedListSummary.py
+++ Summary/LinkedListSummary.py
@@ -11,8 +11,14 @@ n2 = ListNode(3)
 head.next = n1
 n1.next = n2
 
+head2 = ListNode(4)
+n3 = ListNode(5)
+n4 = ListNode(6)
+head2.next = n3
+n3.next = n4
 
 class Solution:
+    level = 1
     def printList(self, head):
         while head is not None:
             print head.val
@@ -58,5 +64,45 @@ class Solution:
 
         return slow
 
+    def getMiddle(self, head):
+        if head is None or head.next is None:
+            return head
+        quick = head
+        slow = head
+
+        while quick.next is not None and quick.next.next is not None:
+            quick = quick.next
+            slow = slow.next
+        return slow
+
+    def mergeTwoSortList(self, head1, head2):
+        if head1 is None:
+            return head2
+        if head2 is None:
+            return head1
+        if head1.val < head2.val:
+            mergeHead = head1
+            head1 = head1.next
+        else:
+            mergeHead = head2
+            head2 = head2.next
+        cur = mergeHead
+        while head1 is not None and head2 is not None:
+            if head1.val < head2.val:
+                cur.next = head1
+                head1 = head1.next
+                cur = cur.next
+            else:
+                cur.next = head2
+                head2 = head2.next
+                cur = cur.next
+        if head1 is not None:
+            cur.next = head1
+        elif head2 is not None:
+            cur.next = head2
+
+        return mergeHead
+
 a = Solution()
-print a.getLastKth(head, 2).val
+new = a.mergeTwoSortList(head, head2)
+a.printList(new)
