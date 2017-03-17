ba80e872cdd6c39c5e4dbde01dd8c576024d8815
pengshuang <pengshuang92@163.com>
2017-03-17 10:02:44 +0800

    修改

--- Union Find/Number of Islands.py
+++ Union Find/Number of Islands.py
@@ -58,73 +58,8 @@ class Solution(object):
                 self.dfs(grid, i, j)
         return ans
 
-class Solution2:
-    # @param grid, a list of list of characters
-    # @return an integer
-    def numIslands(self, grid):
-        m = len(grid)
-        if m == 0:
-            return 0
-        n = len(grid[0])
-        visit = [[False for i in range(n)]for j in range(m)]
-        def check(x, y):
-            if x >= 0 and x<m and y>= 0 and y< n and grid[x][y] == '1' and visit[x][y] == False:
-                return True
-        def dfs(x, y):
-            nbrow = [1,0,-1,0]
-            nbcol = [0,1,0,-1]
-            for k in range(4):
-                newx = x + nbrow[k]
-                newy = y + nbcol[k]
-                if check(newx, newy):
-                    visit[newx][newy] = True
-                    dfs(newx,newy)
-        count = 0
-        for row in range(m):
-            for col in range(n):
-                if check(row,col):
-                    visit[row][col] = True
-                    dfs(row,col)
-                    count+=1
-        return count
 
-class Solution3(object):
-    m, n = None, None
-    def dfs(self, visit, i, j):
-        if i < 0 or i >= self.m or j < 0 or j >= self.n:
-            return
-        if visit[i][j]:
-            visit[i][j] = False
-            self.dfs(visit, i-1, j)
-            self.dfs(visit, i+1,j)
-            self.dfs(visit, i, j-1)
-            self.dfs(visit, i, j+1)
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        self.m = len(grid)
-        if self.m == 0:
-            return 0
-        self.n = len(grid[0])
-        if self.n == 0:
-            return 0
-        visit = [[False for i in range(self.n)] for j in range(self.m)]
-        ans = 0
-        for i in xrange(self.m):
-            for j in xrange(self.n):
-                if grid[i][j] == "1":
-                    visit[i][j] = True
-        for i in xrange(self.m):
-            for j in xrange(self.n):
-                if not visit[i][j]:
-                    continue
-                ans += 1
-                self.dfs(visit, i, j)
-        return ans
-
-solution = Solution3()
+solution = Solution()
 value = ["10000","11010","11000","00000"]
 print solution.numIslands(value)
 
--- test.py
+++ test.py
@@ -1,9 +1,49 @@
-import sys
-
-while 1:
-    n = raw_input()
-    arr = (raw_input().split(" "))
-    print arr
+class Node:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
+def minPathSum(grid):
+    m, n = len(grid), len(grid[0])
+    dp = [[1 << 32] * n for _ in range(m)]
+    dp[0][0] = grid[0][0]
 
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] != "x":
+                bfs(grid, dp, i, j)
+
+    return dp[m - 1][n - 1]
+
+
+def bfs(grid, dp, x, y):
+    direX = [1, 0, 0, -1]
+    direY = [0, -1, 1, 0]
+    m, n = len(grid), len(grid[0])
+    visited = [[False] * n for _ in range(m)]
+    visited[x][y] = True
+    queue = []
+    queue.append(Node(x, y))
+
+    while len(queue) > 0:
+        node = queue.pop(0)
+        for i in range(4):
+            newNode = Node(node.x + direX[i], node.y + direY[i])
+            if not inGraph(newNode, grid) or visited[newNode.x][newNode.y] \
+                    or grid[newNode.x][newNode.y] == "x":
+                continue
+            dp[newNode.x][newNode.y] = min(dp[newNode.x][newNode.y], grid[newNode.x][newNode.y] + dp[node.x][node.y])
+            queue.append(Node(newNode.x, newNode.y))
+            visited[newNode.x][newNode.y] = True
+
+def inGraph(node, grid):
+    if node.x < 0 or node.x >= len(grid):
+        return False
+    elif node.y < 0 or node.y >= len(grid[0]):
+        return False
+    else:
+        return True
+
+grid = [[1,1,1], [1,2,"x"], [0,1,1]]
+print minPathSum(grid)
